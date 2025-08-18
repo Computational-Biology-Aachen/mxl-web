b4c5763b02469db2f62d60aa7ff79e7d7dd32813
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-08-18 13:53:37 +0200

    dev: new: npq model as web-assembly

--- src/routes/npq/+page.svelte
+++ src/routes/npq/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import LineChart from "$lib/chartjs/lineChart.svelte";
   import { kvaerno45 } from "$lib/integrators/implicit/kvaerno45";
+  import { wa_npq } from "$lib/pkg/wa_integrate";
   import { model } from "./model";
 
   function arrayColumn<T>(arr: Array<Array<T>>, n: number): Array<T> {
@@ -11,7 +12,8 @@
   let yLim = $state(8);
 
   let result = $derived.by(() => {
-    return kvaerno45(model, {
+    let tStart = Date.now();
+    let res = kvaerno45(model, {
       initialValues: [
         1.6999999999999997, 4.706348349506148, 3.9414515288091567,
         3.7761613271207324, 7.737821100836988, 0.5105293511676007,
@@ -22,28 +24,24 @@
       rtol: 1e-4,
       atol: 1e-4,
     });
-    // return rk45(model, {
-    // 	initialValues: [
-    // 		1.6999999999999997, 4.706348349506148, 3.9414515288091567, 3.7761613271207324,
-    // 		7.737821100836988, 0.5105293511676007, 0.5000000001374878, 0.09090909090907397
-    // 	],
-    // 	tEnd: 50,
-    // 	pars: [ppfd]
-    // });
-
-    // return euler(model, {
-    // 	initialValues: [
-    // 		1.6999999999999997, 4.706348349506148, 3.9414515288091567, 3.7761613271207324,
-    // 		7.737821100836988, 0.5105293511676007, 0.5000000001374878, 0.09090909090907397
-    // 	],
-    // 	tEnd: 50,
-    // 	stepSize: 0.01,
-    // 	pars: [ppfd]
-    // });
+    console.log(`Javascript Integration took ${Date.now() - tStart} ms`);
+    return res;
+  });
+  let result2 = $derived.by(() => {
+    let tStart = Date.now();
+    let res = wa_npq(
+      [
+        1.6999999999999997, 4.706348349506148, 3.9414515288091567,
+        3.7761613271207324, 7.737821100836988, 0.5105293511676007,
+        0.5000000001374878, 0.09090909090907397,
+      ],
+      [ppfd],
+    );
+    console.log(`WebAssembly Integration took ${Date.now() - tStart} ms`);
+    return res;
   });
 
   let lineData = $derived.by(() => {
-    console.log(result.time.length);
     return {
       labels: result.time,
       datasets: [
@@ -82,12 +80,53 @@
       ],
     };
   });
+
+  let lineData2 = $derived.by(() => {
+    return {
+      labels: result2.time,
+      datasets: [
+        {
+          label: "ATP",
+          data: arrayColumn(result2.values, 0),
+        },
+        {
+          label: "Plastoquinone (oxidised)",
+          data: arrayColumn(result2.values, 1),
+        },
+        {
+          label: "Plastocyanine (oxidised)",
+          data: arrayColumn(result2.values, 2),
+        },
+        {
+          label: "Ferredoxine",
+          data: arrayColumn(result2.values, 3),
+        },
+        {
+          label: "protons_lumen",
+          data: arrayColumn(result2.values, 4),
+        },
+        {
+          label: "Light-harvesting complex",
+          data: arrayColumn(result2.values, 5),
+        },
+        {
+          label: "PsbS (de-protonated)",
+          data: arrayColumn(result2.values, 6),
+        },
+        {
+          label: "Violaxanthin",
+          data: arrayColumn(result2.values, 7),
+        },
+      ],
+    };
+  });
 </script>
 
 <h1>Non-photochemical quenching</h1>
 <p>Quick and dirty demo to get ODE integration running on the client-side.</p>
 
 <LineChart data={lineData} {yLim} />
+<LineChart data={lineData2} {yLim} />
 <div>
   <label>
     <span>PPFD</span>
