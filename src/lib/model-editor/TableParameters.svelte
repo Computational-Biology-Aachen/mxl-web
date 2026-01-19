fed6e2921d9017523485f7c5e9d439b559b9c1dd
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-01-19 15:38:05 +0100

    dev: new: model editor

---
+++ src/lib/model-editor/TableParameters.svelte
@@ -0,0 +1,31 @@
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Initial value</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>p1</td>
+      <td>1.0</td>
+    </tr>
+    <tr>
+      <td>p2</td>
+      <td>2.0</td>
+    </tr>
+  </tbody>
+</table>
+<button>+ add new item</button>
+
+<style>
+  table {
+    font-size: 0.75rem;
+  }
+  button {
+    font-size: 0.75rem;
+    width: 8rem;
+    color: var(--pico-background-color);
+    background-color: var(--pico-color);
+  }
+</style>
---
+++ src/lib/model-editor/TableReactions.svelte
@@ -0,0 +1,42 @@
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Rate law</th>
+      <th>Stoichiometry</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>rxn1</td>
+      <td>
+        <select>
+          <option>A</option>
+        </select>
+      </td>
+      <td>...</td>
+    </tr>
+    <tr>
+      <td>rxn2</td>
+      <td>
+        <select>
+          <option>A</option>
+        </select>
+      </td>
+      <td>...</td>
+    </tr>
+  </tbody>
+</table>
+<button>+ add new item</button>
+
+<style>
+  table {
+    font-size: 0.75rem;
+  }
+  button {
+    font-size: 0.75rem;
+    width: 8rem;
+    color: var(--pico-background-color);
+    background-color: var(--pico-color);
+  }
+</style>
