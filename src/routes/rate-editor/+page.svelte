b6194d2beaf9987cbf7cb4601a25b9f03c7a976b
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-01-27 07:55:46 +0100

    dev: fix: minor styling

---
+++ src/routes/rate-editor/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Divide, Number, Symbol } from "$lib/MathMl";
+
+  const choices = [Symbol, Number, Divide];
+</script>
+
+<h2>Edit equation</h2>
+<div></div>
+
+<style>
+</style>
