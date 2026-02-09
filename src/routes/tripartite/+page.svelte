b516a806497e08e51edf307321a681349a5be710
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-02-09 15:55:56 +0100

    dev: fix: rebase

--- src/routes/tripartite/+page.svelte
+++ src/routes/tripartite/+page.svelte
@@ -1,107 +1,204 @@
 <script lang="ts">
-  import LineChart from "$lib/chartjs/lineChart.svelte";
-  import { euler } from "$lib/integrators/explicit/euler";
+  import Icon from "$lib/Icon.svelte";
   import Math from "$lib/Math.svelte";
+  import { Minus, Mul, Name, Num } from "$lib/mathml";
+  import { ModelBuilder } from "$lib/model-editor/model";
+  import ModelEditorPopover from "$lib/model-editor/ModelEditorPopover.svelte";
+  import Simulator from "$lib/Simulator.svelte";
   import Slider from "$lib/Slider.svelte";
-  import {
-    jsWorkerManager,
-    pyWorkerManager,
-    type WorkerManager,
-  } from "$lib/stores/workerStore";
-  import { arrayColumn } from "$lib/utils";
-  import { onMount } from "svelte";
-  import { modelJs } from "./modelJs";
-  import { modelPy } from "./modelPy";
 
   // Simulation state
   let tEnd = $state(100);
   let simulatorComponent: Simulator;
 
-  // Eq.
-  // dP/dt = r_p·P − α·P·C − β·P·M − η·P^2
-  // dC/dt = α·P·C − ν·C^2
-  // dM/dt = r_m·M − β·M·P − γ·M^2
+  function initModel(): ModelBuilder {
+    return new ModelBuilder()
+      .addVariable("Public", {
+        value: 1.0,
+        slider: {
+          min: "0.0",
+          max: "10000.0",
+          step: "1",
+        },
+      })
+      .addVariable("Cheater", {
+        value: 1.0,
+        slider: {
+          min: "0.0",
+          max: "10000.0",
+          step: "1",
+        },
+      })
+      .addVariable("Private", {
+        value: 1.0,
+        slider: {
+          min: "0.0",
+          max: "10000.0",
+          step: "1",
+        },
+      })
+      .addParameter("r_p", {
+        value: 0.4,
+        slider: {
+          desc: "(growth rate)",
+          min: "0.0",
+          max: "1.0",
+          step: "0.00001",
+        },
+      })
+      .addParameter("eta", {
+        value: 0.0001,
+        slider: {
+          desc: "(density)",
+          min: "0.0",
+          max: "1.0",
+          step: "0.00001",
+        },
+      })
+      .addParameter("nu", {
+        value: 0.00001,
+        slider: {
+          desc: "(density)",
+          min: "0.0",
+          max: "1.0",
+          step: "0.00001",
+        },
+      })
+      .addParameter("r_m", {
+        value: 0.2,
+        slider: {
+          desc: "(growth rate)",
+          min: "0.0",
+          max: "5.0",
+          step: "0.0001",
+        },
+      })
+      .addParameter("gamma", {
+        value: 0.0001,
+        slider: {
+          desc: "(density)",
+          min: "0.0",
+          max: "5.0",
+          step: "0.0001",
+        },
+      })
+      .addParameter("alpha", {
+        value: 0.0002,
+        slider: {
+          desc: "(P→C cooperation)",
+          min: "0.0",
+          max: "1.0",
+          step: "0.0001",
+        },
+      })
+      .addParameter("beta", {
+        value: 0.0001,
+        slider: {
+          desc: "(P↔M competition)",
+          min: "0.0",
+          max: "1.0",
+          step: "0.0001",
+        },
+      })
+      .addReaction("dPdt", {
+        fn: new Minus([
+          new Mul([new Name("r_p"), new Name("Public")]),
+          new Mul([new Name("alpha"), new Name("Public"), new Name("Cheater")]),
+          new Mul([new Name("beta"), new Name("Public"), new Name("Private")]),
+          new Mul([
+            new Name("eta"),
+            // FIXME: square
+            new Name("Public"),
+            new Name("Public"),
+          ]),
+        ]),
+        stoichiometry: [{ name: "Public", value: new Num(1.0) }],
+      })
+      .addReaction("dCdt", {
+        fn: new Minus([
+          new Mul([new Name("alpha"), new Name("Public"), new Name("Cheater")]),
+          new Mul([
+            new Name("nu"),
+            // FIXME: square
+            new Name("Cheater"),
+            new Name("Cheater"),
+          ]),
+        ]),
+        stoichiometry: [{ name: "Cheater", value: new Num(1.0) }],
+      })
+      .addReaction("dMdt", {
+        fn: new Minus([
+          new Mul([new Name("r_m"), new Name("Private")]),
+          new Mul([new Name("beta"), new Name("Public"), new Name("Private")]),
+          new Mul([
+            new Name("gamma"),
+            // FIXME: Square
+            new Name("Private"),
+            new Name("Private"),
+          ]),
+        ]),
+        stoichiometry: [{ name: "Private", value: new Num(1.0) }],
+      });
+  }
+
+  let model = $state(initModel());
+
   const eqs = String.raw`
   \begin{align*}
     \frac{dP}{dt} &= r_p\,P - \alpha\,P\,C - \beta\,P\,M - \eta\,P^2 \\
     \frac{dC}{dt} &= \alpha\,P\,C - \nu\,C^2 \\
     \frac{dM}{dt} &= r_m\,M - \beta\,M\,P - \gamma\,M^2 \\
-  \end{align*}
-    `;
-  function model(t: number, vars: number[], pars: number[]) {
-    const [P, C, M] = vars;
-    const [r_p, r_m, alpha, beta, eta, gamma, nu] = pars;
+  \end{align*}`;
 
-    const dPdt = r_p * P - alpha * P * C - beta * P * M - eta * P * P;
-    const dCdt = alpha * P * C - nu * C * C;
-    const dMdt = r_m * M - beta * M * P - gamma * M * M;
-
-    return [dPdt, dCdt, dMdt];
-  }
-
-  let result = $derived.by(() => {
-    // return rk45(model, {
-    // 	initialValues: [e0, c0],
-    // 	tEnd: tEnd,
-    // 	pars: [mu_e, mu_c, a_e, a_c, delta_e, theta]
-    // });
-    return euler(model, {
-      initialValues: [p0, c0, m0],
-      tEnd: tEnd,
-      pars: [r_p, r_m, alpha, beta, eta, gamma, nu],
-      method: "LSODA",
-    });
-  }
-
-  onMount(() => {
-    // Set up message handlers for this component
-    const unsubscribePy = pyWorker.onMessage((data) => {
-      if (backend.worker === pyWorker) {
-        result = data;
-      }
-    });
-
-    const unsubscribeJs = jsWorker.onMessage((data) => {
-      if (backend.worker === jsWorker) {
-        result = data;
-      }
-    });
-
-    // Set default backend
-    backend = backends[1].backend;
-
-    // Initial run
-    runSimulation();
-
-    // Cleanup handlers (workers are shared so don't terminate them)
-    return () => {
-      unsubscribePy();
-      unsubscribeJs();
-    };
+  let parSliders = $derived.by(() => {
+    return model.parameters
+      .entries()
+      .filter((k) => k[1].slider !== undefined)
+      .map(([name, par]) => {
+        return {
+          name: name,
+          init: par.value,
+          min: par.slider!.min,
+          max: par.slider!.max,
+          step: par.slider!.step,
+        };
+      })
+      .toArray();
   });
 
-  let lineData = $derived.by(() => {
-    return {
-      labels: result.time,
-      datasets: [
-        {
-          label: "Public",
-          data: arrayColumn(result.values, 0),
-        },
-        {
-          label: "Cheaters",
-          data: arrayColumn(result.values, 1),
-        },
-        {
-          label: "Private",
-          data: arrayColumn(result.values, 2),
-        },
-      ],
-    };
+  let varSliders = $derived.by(() => {
+    return model.variables
+      .entries()
+      .filter((k) => k[1].slider !== undefined)
+      .map(([name, par]) => {
+        return {
+          name: name,
+          init: par.value,
+          min: par.slider!.min,
+          max: par.slider!.max,
+          step: par.slider!.step,
+        };
+      })
+      .toArray();
   });
 </script>
 
-<h1>Tripartite population dynamics</h1>
+<div class="topbar">
+  <div class="breadcrumbs">
+    <a class="light" href="/">Models</a>
+    <span class="light">/</span>
+    <span class="bold">Tripartite-population-dynamics</span>
+  </div>
+  <div class="row">
+    <button
+      onclick={() => {
+        model = initModel();
+        simulatorComponent.runSimulation(model);
+      }}>Reset</button
+    >
+    <button popovertarget="model-editor">Edit model</button>
+  </div>
+</div>
 
 <p>
   Dynamic model of a tripartite population of <b>P</b>ublic consumers,
@@ -112,153 +209,130 @@
 
 <Math tex={eqs} display />
 
-<h3>Initial conditions & settings</h3>
-<div class="grid-row">
-  <span>Initial conditions</span>
-  <div class="inner-row">
-    <Slider
-      name="Public"
-      bind:val={p0}
-      callback={runSimulation}
-      min="0.0"
-      max="10000.0"
-      step="1"
-    />
-    <Slider
-      name="Cheaters"
-      bind:val={c0}
-      callback={runSimulation}
-      min="0.0"
-      max="10000.0"
-      step="1"
-    />
-    <Slider
-      name="Private"
-      bind:val={m0}
-      callback={runSimulation}
-      min="0.0"
-      max="10000.0"
-      step="1"
-    />
-    <Slider
-      name="Simulate until t"
-      bind:val={tEnd}
-      callback={runSimulation}
-      min="1.0"
-      max="10000.0"
-      step="10"
-    />
+{#if parSliders.length > 0}
+  <div class="heading">
+    <Icon>tune</Icon>
+    <h3>Simulation parameters</h3>
   </div>
-</div>
+  <div class="grid-row">
+    {#each parSliders as par, idx (par.name)}
+      <Slider
+        name={par.name}
+        callback={() => simulatorComponent.runSimulation(model)}
+        bind:val={
+          () => {
+            return model.parameters.get(par.name)!.value;
+          },
+          (val) => {
+            let old = model.parameters.get(par.name)!;
 
-<div class="grid-row">
-  <span>Public (P)</span>
-  <div class="inner-row">
-    <Slider
-      name="r_p (growth rate)"
-      bind:val={r_p}
-      callback={runSimulation}
-      min="0.0"
-      max="1.0"
-      step="0.00001"
-    />
-    <Slider
-      name="η (density)"
-      bind:val={eta}
-      callback={runSimulation}
-      min="0.0"
-      max="1.0"
-      step="0.00001"
-    />
+            model.parameters = model.parameters.set(par.name, {
+              ...old,
+              value: val,
+            });
+          }
+        }
+        min={par.min}
+        max={par.max}
+        step={par.step}
+      />
+    {/each}
   </div>
-</div>
+{/if}
 
-<div class="grid-row">
-  <span>Cheaters (C)</span>
-  <div class="inner-row">
-    <Slider
-      name="ν (density)"
-      bind:val={nu}
-      callback={runSimulation}
-      min="0.0"
-      max="1.0"
-      step="0.00001"
-    />
+{#if varSliders.length > 0}
+  <div class="heading">
+    <Icon>tune</Icon>
+    <h3>Initial conditions</h3>
   </div>
-</div>
-<div class="grid-row">
-  <span>Private (M)</span>
-  <div class="inner-row">
-    <Slider
-      name="r_m (growth rate)"
-      bind:val={r_m}
-      callback={runSimulation}
-      min="0.0"
-      max="5.0"
-      step="0.0001"
-    />
-    <Slider
-      name="γ (density)"
-      bind:val={gamma}
-      callback={runSimulation}
-      min="0.0"
-      max="5.0"
-      step="0.0001"
-    />
+  <div class="grid-row">
+    {#each varSliders as { name, min, max, step }, idx}
+      <Slider
+        {name}
+        callback={() => simulatorComponent.runSimulation(model)}
+        bind:val={
+          () => {
+            return model.variables.get(name)!.value;
+          },
+          (val) => {
+            let old = model.variables.get(name)!;
+            model.variables = model.variables.set(name, { ...old, value: val });
+          }
+        }
+        {min}
+        {max}
+        {step}
+      />
+    {/each}
   </div>
-</div>
-<div class="grid-row">
-  <span>Interaction</span>
-  <div class="inner-row">
-    <Slider
-      name="α (P→C cooperation)"
-      bind:val={alpha}
-      callback={runSimulation}
-      min="0.0"
-      max="1.0"
-      step="0.0001"
-    />
-    <Slider
-      name="β (P↔M competition)"
-      bind:val={beta}
-      callback={runSimulation}
-      min="0.0"
-      max="1.0"
-      step="0.0001"
-    />
-  </div>
-</div>
+{/if}
+<Simulator bind:this={simulatorComponent} {model} {tEnd}></Simulator>
 
-<LineChart data={lineData} yMax={yLim} />
-
-<label for="backend-select">Choose an integration backend:</label>
-<select
-  id="backend-select"
-  bind:value={backend}
-  onchange={() => {
-    runSimulation();
+<ModelEditorPopover
+  parent={model}
+  onSave={(edited) => {
+    model = edited;
+    simulatorComponent.runSimulation(model);
   }}
->
-  {#each backends as item}
-    <option value={item.backend}>
-      {item.name}
-    </option>
-  {/each}
-</select>
+/>
 
 <style>
-  span {
-    font-size: 0.8rem;
-    font-weight: 700;
+  .topbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+  .breadcrumbs {
+    display: flex;
+    gap: 0.5rem;
+    font-size: var(--text-sm);
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  .heading {
+    display: flex;
+    gap: 0.5rem;
   }
   .grid-row {
     display: grid;
-    grid-template-columns: 5rem 1fr;
-  }
-  .inner-row {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 10rem);
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     align-items: center;
-    gap: 0 1rem;
+    box-shadow: var(--shadow);
+    border-radius: 0.75rem;
+    background-color: var(--bg-l1);
+    padding: 1.5rem;
+  }
+  .bold {
+    font-weight: 600;
+  }
+  .light {
+    color: var(--slate-400);
+    font-weight: 400;
+  }
+  a.light:hover {
+    color: var(--primary);
+  }
+  button {
+    cursor: pointer;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: var(--primary);
+    padding: 0 1rem;
+    min-width: 1rem;
+    height: 2rem;
+    color: white;
+    font-weight: var(--weight-bold);
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    letter-spacing: 0.025em;
+  }
+  button:hover {
+    background-color: lch(from var(--primary) calc(l - 20) c h);
   }
 </style>
