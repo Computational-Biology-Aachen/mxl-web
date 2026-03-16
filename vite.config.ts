c1df8d4e2a47b460ea29691bb5e3a2c8ca8e2975
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-03-16 08:33:15 +0000

    dev: fix: removed unnecessary deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import devtoolsJson from "vite-plugin-devtools-json";
-import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [sveltekit(), wasm(), devtoolsJson(), topLevelAwait()],
+  plugins: [sveltekit(), wasm()],
   worker: {
     format: "es",
   },
