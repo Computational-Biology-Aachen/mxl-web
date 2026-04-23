d17eed89cc1218fb1dce35750406457e01099ac7
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-04-23 14:29:26 +0200

    feat: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+  import { page } from "$app/stores";
+  import Icon from "$lib/Icon.svelte";
+</script>
+
+<svelte:head>
+  <title>{$page.status} — mxlweb</title>
+</svelte:head>
+
+<hgroup>
+  <h1>{$page.status}</h1>
+  <p>{$page.error?.message ?? "An unexpected error occurred"}</p>
+</hgroup>
+
+<div class="card">
+  <div class="row">
+    <Icon color="primary">home</Icon>
+    <a href="{base}/">Back to home</a>
+  </div>
+</div>
+
+<style>
+  .card {
+    box-shadow: var(--shadow);
+    border-radius: var(--border-radius);
+    background-color: var(--bg-l1);
+    padding: 1.5rem;
+  }
+
+  .row {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+</style>
