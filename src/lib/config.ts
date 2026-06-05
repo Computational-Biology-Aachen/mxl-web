002bbc7b7b1333ca42adb16f60c996e13a3ca0fc
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-06-05 10:25:33 +0200

    fix: unify config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,4 +4,4 @@ export const title = "mxlweb";
 export const description = "Toy implementation of client-side ODE integration";
 export const url = dev
   ? "http://localhost:5173/"
-  : "https://https://computational-biology-aachen.github.io/mxl-web/";
+  : "https://computational-biology-aachen.github.io/mxl-web/";
