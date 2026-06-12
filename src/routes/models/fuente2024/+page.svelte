20fb61de4a1e25e2208a4e34948a4340ed6e2263
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-06-12 10:59:07 +0200

    feat: added fuente 2024 model

---
+++ src/routes/models/fuente2024/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { backends, type Analyses } from "$lib";
+  import AnalysesDashboard from "$lib/AnalysesDashboard.svelte";
+  import { SectionMain as Main } from "@computational-biology-aachen/design";
+  import { initModel } from "./model";
+
+  let analyses: Analyses = $state([
+    {
+      type: "simulation" as const,
+      id: 0,
+      idx: 0,
+      title: "Simulation",
+      col: 1,
+      span: 3,
+      tEnd: 100,
+      xMin: undefined,
+      xMax: undefined,
+      yMin: undefined,
+      yMax: undefined,
+      timeoutInSeconds: 20,
+      backend: backends.wasmRadau5,
+      nTimePoints: 100,
+      lineDisplay: "last",
+    },
+  ]);
+</script>
+
+<svelte:head>
+  <title>Fuente 2024 - mxlweb</title>
+</svelte:head>
+
+<Main
+  pad="tight"
+  width="full"
+>
+  <AnalysesDashboard
+    name="Fuente 2024"
+    initModel={initModel}
+    bind:analyses={analyses}
+    equationsOpen={false}
+  >
+    <h1>Fuente 2024 model</h1>
+  </AnalysesDashboard>
+</Main>
